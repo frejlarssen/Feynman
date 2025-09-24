a845f16ee148dc986701ca0459a1a1e688bbb6af
Frej Larssen <frejlarssen@hotmail.se>
2025-09-24 18:02:44 +0200

    some notes

--- circuits/quantum_walk_generator.py
+++ circuits/quantum_walk_generator.py
@@ -1,6 +1,7 @@
 n = 4
 it = 2
-# n=4 and it=2 is fast. it=3 (23 internal wires) takes time...
+# n=4 and it=2 is fast. it=3 (23 internal wires) takes long time with breaking over deterministic gates.
+# n=4 fast when joining over deterministic gates.
 
 def cinc(n):
     for i in range(n-1):
