1374f581da89a1c87b5a82606e29649c8d4ecbd0
Frej Larssen <frejlarssen@hotmail.se>
2025-10-15 15:43:50 +0200

    Forgot to add this to latest commit

--- src/simulator.h
+++ src/simulator.h
@@ -10,111 +10,10 @@
 #include <atomic>
 #include <cstdlib>
 #include <mutex>	
-#include "src/circuit.h"
-#include "src/parallel_for.h"
+#include "circuit.h"
+#include "parallel_for.h"
 
 #define fLIMIT 0.9999999
-
-using namespace std;
-
-struct Options {
-    string circuit_file;
-    vector<bool> input_bits;
-    vector<bool> output_bits;
-    int num_chunk1 = -1;
-    int num_chunk2 = -1;
-    float fraction = 1.0;
-    bool only_build = false;
-};
-
-const vector<bool> bit_array_from_string(const string& s) {
-    vector<bool> bits(s.size());
-    for (size_t i = 0; i < s.size(); i++) {
-        if (s.at(i) == '1') {
-            bits.at(s.size() -1 - i) = true;
-        } else if (s.at(i) == '0') {
-            bits.at(s.size() -1 - i) = false;
-        } else {
-            cerr << "Invalid bitstring!" << endl;
-            exit(1);
-        }
-    }
-    return bits;
-}
-
-const string string_from_bit_array(const vector<bool> bit_arr) {
-    string str = "";
-    for (int i = 0; i < bit_arr.size(); i++) {
-        if (bit_arr.at(bit_arr.size()-1 - i)) {
-            str += "q" + to_string(bit_arr.size()-1 - i) + "=1";
-        } else {
-            str += "q" + to_string(bit_arr.size()-1 - i) + "=0";
-        }
-    }
-    return str;
-}
-
-Options get_options(int argc, char* argv[]) {
-    Options opts;
-
-    const char* helpstr = "Usage: ./feynqft -c circuit_file -i input_bitstring -o output_bitstring -p num_chunk1 -r num_chunk2 (-B)\n";
-
-    if (argc < 4) {
-        cout << helpstr;
-        exit(1);
-    }
-
-    int k;
-
-    auto to_int = [](const std::string& word) -> unsigned {
-        return std::atoi(word.c_str());
-    };
-
-    auto to_float = [](const std::string& word) -> float {
-        return std::atof(word.c_str());
-    };
-
-    //cout << "Parsing options" << endl;
-    //cout << "argc: " << argc << endl;
-    //cout << "argv: ";
-    for (int i = 0; i < argc; i++) {
-        cout << argv[i] << " ";
-    }
-    cout << endl;
-
-    while ((k = getopt(argc, argv, "c:i:o:p:r:f:B")) != -1) {
-        switch (k) {
-          case 'c':
-            opts.circuit_file = optarg;
-            break;
-          case 'i': //Not necessary. We could leave to the user to initialize in circuit_file and always start at state |0>.
-            opts.input_bits = bit_array_from_string(optarg);
-            break;
-          case 'o':
-            opts.output_bits = bit_array_from_string(optarg);
-            break;
-          case 'p':
-            opts.num_chunk2 = to_int(optarg);
-            break;
-          case 'r':
-            opts.num_chunk1 = to_int(optarg);
-            break;
-          case 'f':
-            cout << "f optarg: " << optarg << endl;
-            opts.fraction = to_float(optarg);
-            printf("opts.fraction: %f\n", opts.fraction);
-            cout << "opts.fraction: " << opts.fraction << endl;
-            break;
-          case 'B':
-            opts.only_build = true;
-            break;
-          default:
-            cout << helpstr;
-            exit(1);
-        }
-    }
-    return opts;
-}
 
 complex<float> chunk_contribution(const Chunk& chunk, u_int64_t thread/*, std::ostringstream& buf_history*/) {
     complex <float> contribution = 1.0;
@@ -220,7 +119,7 @@ complex<float> chunk_contribution(const Chunk& chunk, u_int64_t thread/*, std::o
 //    complex_add : std::complex<float> : omp_out += omp_in) \
 //    initializer(omp_priv = std::complex<float>(0,0))
 
-complex <float> simulate(Options opts, std::ostringstream& buf, int verbosity = 0) {
+complex <float> simulate(vector<bool> output_bits, vector<bool> input_bits, float fraction, std::ostringstream& buf, int verbosity = 0) {
     // Set values of those reached from natural
     // Implement similarly as FakeRun
 
@@ -235,13 +134,13 @@ complex <float> simulate(Options opts, std::ostringstream& buf, int verbosity = 
     //array<u_int64_t, NUM_CHUNKS+2> histories = {0, 0, 0, 0, 0};
 
     for (const std::shared_ptr<InternalWire>& w : Circuit::output_sources) {
-        w->set_safe_all(1, opts.output_bits.at(w->wire));
+        w->set_safe_all(1, output_bits.at(w->wire));
     }
 
     //cout << "Output set." << endl;
 
     for (const std::shared_ptr<InternalWire>& w : Circuit::input_sources) {
-        if (!w->set_safe_all(1, opts.input_bits.at(w->wire))) { return false; }
+        if (!w->set_safe_all(1, input_bits.at(w->wire))) { return false; }
     }
 
 //    cout << "Output and input set." << endl;
@@ -271,9 +170,9 @@ complex <float> simulate(Options opts, std::ostringstream& buf, int verbosity = 
 
     u_int64_t num_histories_c2 = u_int64_t(1) << num_artificial2;
 
-    cout << "fraction: " << opts.fraction << endl;
+    cout << "fraction: " << fraction << endl;
 
-    size_t num_par_histories = static_cast<size_t>(static_cast<double>(num_histories_c2) * opts.fraction);
+    size_t num_par_histories = static_cast<size_t>(static_cast<double>(num_histories_c2) * fraction);
 
     printf("num_par_histories (to simulate): %lu\n", num_par_histories);
 
@@ -310,7 +209,7 @@ complex <float> simulate(Options opts, std::ostringstream& buf, int verbosity = 
 
         size_t thread_ind = history2_ind; //TODO: Maybe, make one index for each actual thread (from hardware_concurrency) instead of history2?
         u_int64_t history2;
-        if (opts.fraction > fLIMIT) { //TODO: fix this
+        if (fraction > fLIMIT) { //TODO: fix this
             history2 = history2_ind;
         }
         else {
@@ -392,71 +291,4 @@ complex <float> simulate(Options opts, std::ostringstream& buf, int verbosity = 
     });    
 
     return total_amplitude * (float)num_histories_c2 / (float)num_par_histories;
-}
-
-int main(int argc, char* argv[]) {
-
-    Options opts = get_options(argc, argv);
-
-    ParsedCircuit::parse_circuit(opts.circuit_file);
-
-    //printf("Parsed circuit:\n");
-    //printf("  %s\n", ParsedCircuit::parsed_circuit_to_string().c_str());
-
-
-    //TODO: Autotuning: Build circuit many times with different cp-params, save the best params and build once more for those.
-
-    if (opts.num_chunk1 == -1 && opts.num_chunk2 == -1) {
-        Circuit::build_autotuned_circuit();
-    }
-    else if (opts.num_chunk1 > -1 && opts.num_chunk2 > -1) {
-        Circuit::build_circuit(opts.num_chunk1, opts.num_chunk2);
-    }
-    else {
-        cerr << "Both -p and -r must be set, or none of them for autotuning." << endl;
-        exit(1);
-    }
-
-    printf("After build:\n");
-    for (int i = 0; i < NUM_CHUNKS; i++) {
-        printf("Chunk %d with %zu gates and %d artificial sources:\n", i, Circuit::chunks.at(i).gates.size(), Circuit::chunks.at(i).num_artificial);
-        for (const shared_ptr<Gate>& gate: Circuit::chunks.at(i).gates) {
-            printf("  %s\n", gate_to_string(*gate, -1, 2).c_str());
-        }
-    }
-
-    //printf("Output wires after build:\n");
-    //for (shared_ptr<InternalWire>& iw : Circuit::output_sources) {
-    //    printf("%s\n", internal_wire_to_string(iw, 2).c_str());
-    //}
-
-//    printf("Chunks internal wires after build (to reset): \n");
-//    for (int i = 0; i < NUM_CHUNKS; i++) {
-//        printf("Chunk %d with %ld internal wires:\n", i, Circuit::chunks.at(i).internal_wires.size());
-//        for (const shared_ptr<InternalWire>& iw : Circuit::chunks.at(i).internal_wires) {
-//            printf("  %s\n", internal_wire_to_string(iw, -1, 2).c_str());
-//        }
-//    }
-
-    if (opts.only_build) {
-        size_t total_gates = 0;
-        int total_artificial_sources = 0;
-        for (int i = 0; i < NUM_CHUNKS; i++) {
-            total_gates += Circuit::chunks.at(i).gates.size();
-            total_artificial_sources += Circuit::chunks.at(i).num_artificial;
-            printf("Chunk %d: %ld gates, %d artificial sources.\n", i, Circuit::chunks.at(i).gates.size(), Circuit::chunks.at(i).num_artificial);
-        }
-        printf("Total gates: %ld\n", total_gates);
-        printf("Artificial sources: %d\n", total_artificial_sources);
-    }
-    else {
-        std::ostringstream buf;
-
-        // TODO: For benchmarking: Run this many times with different input/output pairs.
-        complex<float> amp = simulate(opts, buf, 3);
-//        int a = simulate();
-        printf("Total amplitude: %f + i%f\n", amp.real(), amp.imag());
-    }
-
-    return 0;
 }
