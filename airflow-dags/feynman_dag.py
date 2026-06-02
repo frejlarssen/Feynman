b2c67e0b5fa81ae460bac6d9631a1ca5a8938d7e
Frej Larssen <frejlarssen@hotmail.se>
2026-06-02 16:22:19 +0200

    dag: Expand over task group

--- airflow-dags/feynman_dag.py
+++ airflow-dags/feynman_dag.py
@@ -1,6 +1,6 @@
 import pendulum
 
-from airflow.sdk import dag, task
+from airflow.sdk import dag, task, task_group
 @dag(
     schedule=None,
     start_date=pendulum.datetime(2026, 1, 1, tz="UTC"),
@@ -12,5 +12,40 @@ def feynman():
     ### Feynman DAG
     The DAG of the Feynman simulator
     """
-    pass
+
+    @task()
+    def split_output_into_batches(output_bitstrings_filename: str):
+        """
+        #### Split into batches
+        Read output bitstrings and split them into batches.
+        The batches are labeled `batch_i.hs`, where i is the number of the batch.
+        """
+
+        # Should we do this right here in python or have a Kubernates pod do it?
+        # Read output_bitstrings_filename
+        # Write batches to the same shared filesystem
+
+        num_batches = 42 #Placeholder
+        batch_ids = range(num_batches)
+        return batch_ids
+
+    @task_group
+    def process_batch(batch_id: int):
+        # Maybe task group is unnecessary. The task we do expand on could be one pod.
+
+        @task()
+        def read_inputs():
+            pass
+
+        @task()
+        def simulate(batch_id: int):
+            pass
+
+        # Kubernates container with docker image. Include read_inputs here also?
+        return simulate(read_inputs())
+
+    batch_ids = split_output_into_batches("output_hexstrings.hs")
+
+    process_batch.expand(batch_id = batch_ids)
+
 feynman()
